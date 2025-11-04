e2ec07232b9173b18342e00ac54971266a281b6e
Sophie <d.a.kimber@icloud.com>
2025-11-04 02:48:30 +0000

    fix reloading bug

--- src/page.go
+++ src/page.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"html"
+	"html/template"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -22,36 +22,22 @@ func postPage(c *gin.Context) {
 	postsMu.RUnlock()
 	if !exists {
 		c.Status(http.StatusNotFound)
-		// serve static 404-ish page
-		staticDir := os.Getenv("STATIC_DIR")
-		if staticDir == "" {
-			staticDir = "../static"
-		}
-		c.File(filepath.Join(staticDir, "post.html"))
 		return
 	}
 
 	baseURL := os.Getenv("BASE_URL")
 	if baseURL == "" {
-		// best effort derive
 		scheme := "http"
-		if strings.EqualFold(c.Request.Header.Get("X-Forwarded-Proto"), "https") || c.Request.TLS != nil {
+		if c.Request.TLS != nil || strings.EqualFold(c.Request.Header.Get("X-Forwarded-Proto"), "https") {
 			scheme = "https"
 		}
 		host := c.Request.Host
-		if host == "" {
-			host = "localhost:8080"
-		}
 		baseURL = fmt.Sprintf("%s://%s", scheme, host)
 	}
 
 	pageURL := fmt.Sprintf("%s/post/%d", baseURL, p.ID)
-	title := html.EscapeString(p.Title)
-	desc := html.EscapeString(p.Description)
-	author := html.EscapeString(p.Author)
 	image := ""
 	if p.Thumbnail != "" {
-		// If thumbnail is already absolute, keep it; else make absolute
 		if strings.HasPrefix(p.Thumbnail, "http://") || strings.HasPrefix(p.Thumbnail, "https://") {
 			image = p.Thumbnail
 		} else {
@@ -59,60 +45,40 @@ func postPage(c *gin.Context) {
 		}
 	}
 
-	// Minimal HTML with OG/Twitter tags and a noscript fallback linking to the JS-rendered page
-	htmlOut := fmt.Sprintf(`<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>%s • Warpdrive</title>
-  <meta property="og:type" content="article" />
-  <meta property="og:url" content="%s" />
-  <meta property="og:title" content="%s" />
-  <meta property="og:description" content="%s" />
-  %s
-  <meta name="twitter:card" content="summary_large_image" />
-  <meta name="twitter:title" content="%s" />
-  <meta name="twitter:description" content="%s" />
-  %s
-  <meta name="author" content="%s" />
-  <meta name="robots" content="index,follow" />
-  <link rel="canonical" href="%s" />
-  <script>location.replace('%s');</script>
-  <noscript>
-    <meta http-equiv="refresh" content="0; url=%s" />
-  </noscript>
-</head>
-<body>
-  <p>Loading post… If you are not redirected, <a href="%s">click here</a>.</p>
-</body>
-</html>`,
-		title,
-		pageURL,
-		title,
-		desc,
-		// og:image
-		func() string {
-			if image != "" {
-				return fmt.Sprintf("<meta property=\"og:image\" content=\"%s\" />", html.EscapeString(image))
-			}
-			return ""
-		}(),
-		title,
-		desc,
-		// twitter:image
-		func() string {
-			if image != "" {
-				return fmt.Sprintf("<meta name=\"twitter:image\" content=\"%s\" />", html.EscapeString(image))
-			}
-			return ""
-		}(),
-		author,
-		pageURL,
-		pageURL,
-		pageURL,
-		pageURL,
-	)
+	meta := fmt.Sprintf(`
+<meta property="og:type" content="article" />
+<meta property="og:url" content="%s" />
+<meta property="og:title" content="%s" />
+<meta property="og:description" content="%s" />
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:title" content="%s" />
+<meta name="twitter:description" content="%s" />
+<meta name="author" content="%s" />
+<link rel="canonical" href="%s" />
+`, pageURL, p.Title, p.Description, p.Title, p.Description, p.Author, pageURL)
 
-	c.Data(http.StatusOK, "text/html; charset=utf-8", []byte(htmlOut))
+	if image != "" {
+		meta += fmt.Sprintf(`<meta property="og:image" content="%s" />
+<meta name="twitter:image" content="%s" />`, image, image)
+	}
+
+	staticDir := os.Getenv("STATIC_DIR")
+	if staticDir == "" {
+		staticDir = "../static"
+	}
+
+	tmpl, err := template.ParseFiles(filepath.Join(staticDir, "post.html"))
+	if err != nil {
+		c.String(http.StatusInternalServerError, "template error: %v", err)
+		return
+	}
+
+	data := map[string]any{
+		"Title":    p.Title,
+		"MetaTags": template.HTML(meta),
+	}
+
+	c.Status(http.StatusOK)
+	c.Header("Content-Type", "text/html; charset=utf-8")
+	_ = tmpl.Execute(c.Writer, data)
 }
